ce09d4eddc0387e983edee5d74f5acc439f6de93
Nilesh Ravindra Manke <nileshmanke12ab@gmail.com>
2024-02-04 17:02:41 +0530

    sample4

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
     <div className='w-11/12 m-auto bg-slate-400'>
     <div className='grid  grid-cols-2  gap-7 sm:grid-cols-3  '>
         
-    <video className='w-[200px]  h-[100px] sm:w-[300px] sm:h-[250px]  m-auto ' src='/videos/Cristiano Ronaldo 2022 ❯ Taki Taki _ Skills & Goals _ HD.mp4'  poster='https://th.bing.com/th/id/OIP.Y6Z1_7PGyG9vFQru_WVKQQHaEK?w=321&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'  controls   ></video>
+    {/* <video className='w-[200px]  h-[100px] sm:w-[300px] sm:h-[250px]  m-auto ' src='/videos/Cristiano Ronaldo 2022 ❯ Taki Taki _ Skills & Goals _ HD.mp4'  poster='https://th.bing.com/th/id/OIP.Y6Z1_7PGyG9vFQru_WVKQQHaEK?w=321&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'  controls   ></video> */}
 
 
     <video className='w-[200px]  h-[100px] sm:w-[300px] sm:h-[250px]  m-auto  ' src='/videos/pexels-cottonbro-5532762 (2160p).mp4' poster='https://th.bing.com/th/id/OIP.3MY9Ht40AxByRhRGaI88gwHaFj?w=244&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7'    controls   ></video>
